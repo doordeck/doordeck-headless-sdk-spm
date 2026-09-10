6f5182312e44721a3ea439a98c52e533c201dbbc
Doordeck Development <development@doordeck.com>
2026-09-10 12:06:26 +0000

    Checksum and version update v2.7.0

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "DoordeckSDK",
-         url: "https://cdn.doordeck.com/xcframework/v2.5.0/DoordeckSDK.xcframework.zip",
-         checksum: "6b9c83b8c13bb4f550e64d0130d475146870315b60085c68d2958804b5fcb205"
+         url: "https://cdn.doordeck.com/xcframework/v2.7.0/DoordeckSDK.xcframework.zip",
+         checksum: "4564b16c7c539d3336a33256638abe8c9cd31c7ce9a7924c52f5ff18bf8c3917"
       )
    ]
 )
